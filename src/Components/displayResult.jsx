62486811265eb0c4c62ac989425ec303f6f1f7bc
raptorf1 <raptor_f1@hotmail.com>
2019-05-13 12:15:44 +0200

    Adds Components folder and creates displayResult.jsx to house the result of the user input.

---
+++ src/Components/displayResult.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class DisplayResult extends Component {
+
+
+  render() {
+    return (
+      <div id="response">
+      </div>
+    )
+  }
+}
+
+export default DisplayResult
