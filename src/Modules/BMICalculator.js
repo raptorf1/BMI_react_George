ffbfa1a30801861532be4872851589fbe9fb1aac
raptorf1 <raptor_f1@hotmail.com>
2019-05-13 15:11:24 +0200

    Adds calculation js file to displayResult component. Edits calculation js file to display correct message even if the BMI value is equal to 18.5, 25 & 30.

--- src/Modules/BMICalculator.js
+++ src/Modules/BMICalculator.js
@@ -22,15 +22,15 @@ const setBMIMessage = (finalBMI) => {
     return "Underweight";
   }
 
-  if (finalBMI > 18.5 && finalBMI < 25) {
+  if (finalBMI >= 18.5 && finalBMI < 25) {
     return "Normal";
   }
 
-  if (finalBMI > 25 && finalBMI < 30) {
+  if (finalBMI >= 25 && finalBMI < 30) {
     return "Overweight";
   }
 
-  if (finalBMI > 30) {
+  if (finalBMI >= 30) {
     return "Obese";
   }
 }
