2ccf7299d508de4ebcaf4a945d8326ca6553b4f0
raptorf1 <raptor_f1@hotmail.com>
2019-05-13 12:00:38 +0200

    Adds labels to house the user input in the App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   render () {
     return (
-      <>
+      <div>
+        
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" />
+        </div>
 
-      </>
+        <div>
+          <label>Height(cm)</label>
+          <input name="height" />
+        </div>
+
+      </div>
     );
   }
 }
