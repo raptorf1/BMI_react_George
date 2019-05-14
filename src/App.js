66bfdaa15ff9fcd8c210d82b6a7d89970bdf5566
raptorf1 <raptor_f1@hotmail.com>
2019-05-14 13:20:38 +0200

    Adds semantic-ui package for styling. Deletes the native logo and css files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DisplayResult from './Components/displayResult';
+import { Container } from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
